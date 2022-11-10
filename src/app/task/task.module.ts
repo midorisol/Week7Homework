89b3f9d327687cb8b95c620aeff6a9bdaf1b06ab
Mark Csoma <84617390+midorisol@users.noreply.github.com>
2022-11-10 17:24:48 +0100

    Homework first part

---
+++ src/app/task/task.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskComponent } from './task/task.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TaskPipe } from './task.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    TaskComponent,
+    TaskListComponent,
+    NewTaskComponent,
+    TaskPipe
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TaskModule { }
